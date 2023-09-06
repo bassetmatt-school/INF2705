799bba6f334e52e94046658e4ffcd5caff98bf02
matt <bassetmatt@gmail.com>
2023-09-06 13:57:50 -0400

    Script to change headers way too refined

--- rename_headers.sh
+++ rename_headers.sh
@@ -1,19 +1,29 @@
-git_hpp () {
+MOVE="mv"
+
+to_hpp () {
     for file in $(find . -name "*.h" -type f); do
-        git mv "$file" "${file%.h}.hpp"
+        $MOVE "$file" "${file%.h}.hpp"
+    done
+    for file in $(find . -name "*.cpp" -o -name "*.hpp" -type f); do
+        sed -i "s|.h\"|.hpp\"|g" $file
+        sed -i "s|<SDL.h>|<SDL2/SDL.h>|g" $file
     done
 }
 
-git_h () {
+to_h () {
     for file in $(find . -name "*.hpp" -type f); do
-        git mv "$file" "${file%.hpp}.h"
+        $MOVE "$file" "${file%.hpp}.h"
+    done
+    for file in $(find . -name "*.cpp" -o -name "*.h" -type f); do
+        sed -i "s|.hpp\"|.h\"|g" $file
+        sed -i "s|<SDL2/SDL.h>|<SDL.h>|g" $file
     done
 }
 
 if [ "$1" == "hpp" ]; then
-    git_hpp
+    to_hpp
 elif [ "$1" == "h" ]; then
-    git_h
+    to_h
 else
     echo "Usage: $0 [hpp|h]"
 fi
