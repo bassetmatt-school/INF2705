782319991932d80bb5a5a54a84fb801d2e5f76b1
matt <bassetmatt@gmail.com>
2023-11-01 14:02:10 -0400

    Update Gouraud shader to take texture into account

--- src/shaders/gouraud.fs.glsl
+++ src/shaders/gouraud.fs.glsl
@@ -8,12 +8,47 @@ in ATTRIB_VS_OUT {
 	vec3 specular;
 } attribIn;
 
+struct Material {
+	vec3 emission;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float shininess;
+};
+
+struct UniversalLight {
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	vec3 position;
+	vec3 spotDirection;
+};
+
+layout (std140) uniform LightingBlock {
+	Material mat;
+	UniversalLight lights[3];
+	vec3 lightModelAmbient;
+	bool useBlinn;
+	bool useSpotlight;
+	bool useDirect3D;
+	bool useTexture;
+	float spotExponent;
+	float spotOpeningAngle;
+};
+
 uniform sampler2D diffuseSampler;
 uniform sampler2D specularSampler;
 
 out vec4 FragColor;
 
 void main() {
-	vec3 color = attribIn.emission + attribIn.ambient + attribIn.diffuse + attribIn.specular;
+	vec3 texDiffuse = vec3(1.0f);
+	vec3 texSpecular = vec3(1.0f);
+	if (useTexture)
+		texDiffuse = texture(diffuseSampler, attribIn.texCoords).rgb;
+		texSpecular = texture(specularSampler, attribIn.texCoords).rgb;
+	vec3 color = attribIn.emission;
+	color += (attribIn.ambient + attribIn.diffuse) * texDiffuse;
+	color += attribIn.specular * texSpecular;
 	FragColor = vec4(clamp(color, 0.0f, 1.0f), 1.0f);
 }
